0689b7cb2feed20487f385fc26a2e2eedc6431ed
christopher cahill and maly phongsavanh <maly@>
2018-10-16 12:09:36 -0700

    addremove class

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,11 @@
 $(document).ready(function() {
   $(".card").click(function() {
+    // $(this).children(".card").addClass("applycolor");
     $(this).children("h3").toggle();
     $(this).children(".definitions").toggle();
+  });
+  $(".col-md-3").click(function() {
+    $(this).children(".card").toggle("applycolor");
+    $(this).children(".card").toggle("");
   });
 });
